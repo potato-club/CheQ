6cc3d57e3ad45c58685e5f6366775130593ba010
seonghun <sdzx0719@naver.com>
2024-05-02 00:23:14 +0900

    feat(add): 프로젝트 생성

---
+++ backend/cheq/src/test/java/potato/cheq/CheqApplicationTests.java
@@ -0,0 +1,13 @@
+package potato.cheq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
