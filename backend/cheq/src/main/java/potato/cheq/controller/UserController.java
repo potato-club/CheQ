3deece1f3466a11fac1eaf94507853ff745bfe2c
seonghun <sdzx0719@naver.com>
2024-05-09 16:41:18 +0900

    add(service): 사용자 입력 회원가입 로직

--- backend/cheq/src/main/java/potato/cheq/controller/UserController.java
+++ backend/cheq/src/main/java/potato/cheq/controller/UserController.java
@@ -3,22 +3,24 @@ package potato.cheq.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import potato.cheq.dto.RequestUserDto;
 import potato.cheq.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/join")
     @ResponseStatus(HttpStatus.OK)
-    public Long getUserData(@RequestBody RequestUserDto dto) throws Exception {
+    public Long saveUserData(@RequestBody RequestUserDto dto) throws Exception {
         return userService.setUserData(dto);
     }
+
+//    @GetMapping("/get")
+//    @ResponseStatus(HttpStatus.OK)
+//    public void getUserData()
 }
