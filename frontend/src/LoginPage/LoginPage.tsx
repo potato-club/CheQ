b63efde362611d3babbe0bc15dcc1aaf5931e73a
jiho <hoo6710@naver.com>
2024-05-16 23:54:16 +0900

    LoginPage Publishing #12

--- frontend/src/LoginPage/LoginPage.tsx
+++ frontend/src/LoginPage/LoginPage.tsx
@@ -1,26 +1,110 @@
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import Hansei from "../LoginImg/hansei.png";
+
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // 정보를 출력하는 코드
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div>
       <StyleLoginPage>
         <AttendanceTitle>
           <MainTitle>CheQ</MainTitle>
         </AttendanceTitle>
+        <HanseiIcon />
+        <LoginForm onSubmit={handleSubmit(onSubmit)}>
+          <FormRow>
+            <label htmlFor="studentid">아이디</label>
+            <input
+              id="studentid" // studentid로 변경
+              type="text"
+              placeholder="학번을 입력해주세요."
+              {...register("studentid", {
+                required: "학번은 필수 입력입니다.",
+                pattern: {
+                  value: /^\d{9}$/,
+                  message: "자신의 학번을 입력해주세요.",
+                },
+              })}
+            />
+            {errors.studentid && (
+              <ErrorMessage>{errors.studentid.message as string}</ErrorMessage>
+            )}
+          </FormRow>
+
+          <FormRow>
+            <label htmlFor="password">비밀번호</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="비밀번호를 입력하세요."
+              {...register("password", {
+                required: "비밀번호는 필수 입력입니다.",
+                pattern: {
+                  value: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/,
+                  message: "영문+숫자 조합 8자 이상 입력해주세요.",
+                },
+              })}
+            />
+            {errors.password && (
+              <ErrorMessage>{errors.password.message as string}</ErrorMessage>
+            )}
+          </FormRow>
+          <LoginBtn>
+            <SubmitButton type="submit">로그인</SubmitButton>
+            <SignUpButton>회원가입</SignUpButton>
+          </LoginBtn>
+        </LoginForm>
       </StyleLoginPage>
     </div>
   );
 };
 
 export default LoginPage;
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding-right: 28px;
+`;
+
+const FormRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+  label {
+    font-size: 18px;
+    color: #cdd6f7;
+    font-weight: bold;
+    margin-bottom: 4px;
+  }
+
+  input {
+    width: 100%;
+    padding: 10px 10px 10px 15px;
+    border: 1px solid #375cde;
+    border-radius: 20px;
+    &::placeholder {
+      /* font-weight: bold; */
+      color: #375cde;
+    }
+  }
+`;
 
 const StyleLoginPage = styled.div`
   display: flex;
   flex-direction: column;
   margin: auto;
-  /* min-width: 200px;
-  max-width: 600px; */
   width: 580px;
-  padding: 0px 20px 73px 20px;
+  padding: 0px 20px 40px 20px;
 `;
 
 const AttendanceTitle = styled.div`
@@ -33,166 +117,52 @@ const MainTitle = styled.h1`
   color: #375cde;
 `;
 
-const AttendanceIcon = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 280px;
-  margin-left: 20px;
-`;
-const IconBox = styled.div`
-  display: flex;
-  align-items: center;
-`;
-const GreenIcon = styled.div`
-  width: 10px;
-  height: 10px;
-  background-color: #22d013;
-  border-radius: 50%;
-`;
-const OrangeIcon = styled.div`
-  width: 10px;
-  height: 10px;
-  background-color: #ffb800;
-  border-radius: 50%;
-`;
-const RedIcon = styled.div`
-  width: 10px;
-  height: 10px;
-  background-color: #ff0000;
-  border-radius: 50%;
-`;
-const GrayIcon = styled.div`
-  width: 10px;
-  height: 10px;
-  background-color: #bdbdbd;
-  border-radius: 50%;
-`;
-const IconTitle = styled.div`
-  font-size: 13px;
-  margin-left: 8px;
-`;
-
-const AttendanceCurrent = styled.div`
-  display: flex;
-  margin-top: 20px;
-`;
-const CurrentBox = styled.div`
-  font-size: 13px;
-  font-weight: bold;
-  margin-left: 10px;
-`;
-
-const AttendanceInfo = styled.div`
+const LoginBtn = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 20px;
-  box-shadow: 0 2px 4px rgba(76, 76, 76, 0), 0 -2px 4px rgba(76, 76, 76, 0.1),
-    2px 0 4px rgba(76, 76, 76, 0.1), -2px 0 4px rgba(76, 76, 76, 0.1);
-  padding: 0px 34px 0px 34px;
-  border-radius: 30px;
-  // 수정
-  /* max-width: 546px;
-  max-height: 600px;
-  overflow-y: auto; */
-`;
-
-const WeekInfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-  height: 30px;
-  padding: 30px 0px 30px 0px;
-  border-bottom: 1px solid #e3e3e3;
-  width: 512px;
-`;
-const InfoBox = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const IconColorR = styled.div`
-  width: 13px;
-  height: 13px;
-  background-color: #ff0000;
-  border-radius: 50%;
-  margin-left: 5px;
-`;
-const IconColorG = styled.div`
-  width: 13px;
-  height: 13px;
-  background-color: #22d013;
-  border-radius: 50%;
-  margin-left: 5px;
-`;
-const IconColorO = styled.div`
-  width: 13px;
-  height: 13px;
-  background-color: #ffb800;
-  border-radius: 50%;
-  margin-left: 5px;
-`;
-const IconColorGray = styled.div`
-  width: 13px;
-  height: 13px;
-  background-color: #bdbdbd;
-  border-radius: 50%;
-  margin-left: 5px;
-`;
-
-const HowWeek = styled.div`
-  margin-left: 15px;
-  font-size: 16px;
-  font-weight: bold;
-`;
-const CurrentDate = styled.div`
-  margin-left: 5px;
-  font-size: 16px;
-  font-weight: bold;
-`;
-const CurrentInfo = styled.div`
-  padding: 30px;
-  font-size: 16px;
-  font-weight: bold;
-`;
-
-const HowWeekGray = styled.div`
-  margin-left: 15px;
-  color: #c9c9c9;
-  font-size: 16px;
-  font-weight: bold;
-`;
-const CurrentDateGray = styled.div`
-  margin-left: 5px;
-  color: #c9c9c9;
-  font-size: 16px;
-  font-weight: bold;
-`;
-const CurrentInfoGray = styled.div`
-  color: #c9c9c9;
-  padding: 30px;
-  font-size: 16px;
-  font-weight: bold;
-`;
-
-const NavBar = styled.div`
-  display: flex;
-  justify-content: space-around;
-  background-color: #375cde;
-  height: 80px;
-  border-radius: 14px;
-  width: 620px;
-  /* min-width: 200px;
-  max-width: 600px; */
   margin: auto;
-  position: fixed;
-  bottom: 0;
-  left: 50%;
-  transform: translateX(-50%);
 `;
-const NavBtn = styled.button`
-  border: none;
-  outline: none;
-  background-color: transparent;
+
+const SubmitButton = styled.button`
+  width: 100%;
+  padding: 12px;
+  background-color: #375cde;
   color: white;
-  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  border-radius: 10px;
+  width: 580px;
   cursor: pointer;
+  margin-bottom: 15px;
+  margin-top: 40px;
+  &:hover {
+    background-color: #2b4caf;
+  }
+`;
+
+const SignUpButton = styled.button`
+  width: 100%;
+  padding: 12px;
+  background-color: white;
+  color: #375cde;
+  font-weight: bold;
+  border: 1px solid #375cde;
+  border-radius: 10px;
+  width: 580px;
+  cursor: default; /* 클릭 비활성화 */
+  pointer-events: none; /* 클릭 이벤트 비활성화 */
+`;
+
+const ErrorMessage = styled.small`
+  margin-top: 5px;
+  color: red;
+  margin-left: 16px;
+`;
+
+const HanseiIcon = styled.img`
+  margin: auto;
+  padding: 70px 0px 70px 0px;
+  width: 400px;
+  height: 400px;
+  content: url(${Hansei});
 `;
