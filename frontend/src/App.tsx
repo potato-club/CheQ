a6122b8ee8f712e42741fc2cbd2cb9700ba49bb7
jiho0419 <127321491+jiho0419@users.noreply.github.com>
2024-05-30 10:00:15 +0900

    라우팅 환경 통합

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import Mainpage from './page/Mainpage';
-import Mypage from './page/Mypage';
+import React from "react";
+import Mainpage from "./page/Mainpage";
+import Mypage from "./page/Mypage";
 import Attendance from "./page/Attendance";
+import Admin from "./page/Admin";
 import Login from "./page/Login";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -10,15 +11,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Mainpage />} />
-        <Route path="/Mypage" element={<Mypage />}/>
-        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<Mainpage />} />
+        <Route path="/Mypage" element={<Mypage />} />
+        <Route path="/" element={<Login />} />
         <Route path="/attendance" element={<Attendance />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
