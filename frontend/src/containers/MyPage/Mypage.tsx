83187488d30e630faf2de531be99fec255bd9c0a
jiho0419 <127321491+jiho0419@users.noreply.github.com>
2024-05-30 09:58:46 +0900

    마이 페이지 코드 정리   #22

--- frontend/src/containers/MyPage/Mypage.tsx
+++ frontend/src/containers/MyPage/Mypage.tsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import Nav from '../../components/NavBar';
-import QRImage from '../../Image/qr -1004.png';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Nav from "../../components/NavBar";
+import QRImage from "../../image/qr -1004.png";
 
 function Mypage() {
   const [userData, setUserData] = useState({
-    studentId: '202110034',
-    department: '컴퓨터공학과',
-    chapel: '3교시',
-    seat: 'H3',
+    studentId: "202110034",
+    department: "컴퓨터공학과",
+    chapel: "3교시",
+    seat: "H3",
   });
 
   // 텍스트 길이를 11자로 제한하는 함수
   const limitText = (text: string) => {
     if (text.length > 11) {
-      return text.substring(0, 11) + '...';
+      return text.substring(0, 11) + "...";
     }
     return text;
   };
@@ -43,21 +43,27 @@ function Mypage() {
             <BoxBMainProfil></BoxBMainProfil>
             <BoxBMaininformation>
               <BoxBMaininformation1>
-                <FixedText>학번 | </FixedText>{userData.studentId}
+                <FixedText>학번 | </FixedText>
+                {userData.studentId}
               </BoxBMaininformation1>
               <BoxBMaininformation2>
-                <FixedText>학과 | </FixedText>{userData.department}
+                <FixedText>학과 | </FixedText>
+                {userData.department}
               </BoxBMaininformation2>
               <BoxBMaininformation3>
-                <FixedText>채플 | </FixedText>{userData.chapel}
+                <FixedText>채플 | </FixedText>
+                {userData.chapel}
               </BoxBMaininformation3>
               <BoxBMaininformation4>
-                <FixedText>좌석 | </FixedText>{userData.seat}
+                <FixedText>좌석 | </FixedText>
+                {userData.seat}
               </BoxBMaininformation4>
             </BoxBMaininformation>
             <BoXBProfilchangeBox>
               <BoxBProfilchangeButton>
-                <BoxBProfilchangeButtontext>정보수정</BoxBProfilchangeButtontext>
+                <BoxBProfilchangeButtontext>
+                  정보수정
+                </BoxBProfilchangeButtontext>
               </BoxBProfilchangeButton>
             </BoXBProfilchangeBox>
           </BoxBMain>
@@ -68,31 +74,28 @@ function Mypage() {
               <BoxCMainText>내 QR 코드</BoxCMainText>
             </BoxCMainTextBox>
             <Line />
-          <BoxCMainQRBox>
-            <BoxCMainQrBoxTag>
-              <BoxCMainQr src={QRImage} alt="QR Code" />
-            </BoxCMainQrBoxTag>
-          </BoxCMainQRBox>
+            <BoxCMainQRBox>
+              <BoxCMainQrBoxTag>
+                <BoxCMainQr src={QRImage} alt="QR Code" />
+              </BoxCMainQrBoxTag>
+            </BoxCMainQRBox>
           </BoxCMain>
-        </BoxC> 
-         <BoxD>
-            <BoxDMain>
-              <BoxDMainA>
-                <BoxDmainAtext>푸쉬 알람설정</BoxDmainAtext>
-              </BoxDMainA>
-              <Line />
-              <BoxDMainB>
-                <BoxDmainBtext>앱 설정</BoxDmainBtext>
-              </BoxDMainB>
-              <Line />
-              <BoxDMainC>
-
-              </BoxDMainC>
-            </BoxDMain>
-        </BoxD>   
+        </BoxC>
+        <BoxD>
+          <BoxDMain>
+            <BoxDMainA>
+              <BoxDmainAtext>푸쉬 알람설정</BoxDmainAtext>
+            </BoxDMainA>
+            <Line />
+            <BoxDMainB>
+              <BoxDmainBtext>앱 설정</BoxDmainBtext>
+            </BoxDMainB>
+            <Line />
+            <BoxDMainC></BoxDMainC>
+          </BoxDMain>
+        </BoxD>
       </BigBox>
       <Nav />
-     
     </div>
   );
 }
@@ -170,8 +173,8 @@ const BoxBMaininformation = styled.div`
   width: 200px;
   height: 110px;
   margin-left: 10px;
-  margin-right: 10px;;
-  `;
+  margin-right: 10px;
+`;
 
 const BoxBMaininformation1 = styled.div`
   display: flex;
@@ -245,7 +248,7 @@ const BoxC = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 5px; 
+  padding: 5px;
 `;
 
 const BoxCMain = styled.div`
@@ -253,7 +256,7 @@ const BoxCMain = styled.div`
   align-items: center;
   flex-direction: column;
   width: 70%;
-  aspect-ratio: 1 / 1; 
+  aspect-ratio: 1 / 1;
   border-radius: 20px;
   box-shadow: 0 2px 4px rgba(76, 76, 76, 0), 0 -2px 4px rgba(76, 76, 76, 0.1),
     2px 0 4px rgba(76, 76, 76, 0.1), -2px 0 4px rgba(76, 76, 76, 0.1);
@@ -269,7 +272,7 @@ const BoxCMainTextBox = styled.div`
 `;
 
 const BoxCMainText = styled.h1`
-  margin-left: 20px;;
+  margin-left: 20px;
   font-size: 12px;
   font-weight: bold;
   color: black;
@@ -278,7 +281,7 @@ const BoxCMainText = styled.h1`
 const Line = styled.div`
   width: 95%;
   height: 2px; //선 두께
-  background-color: #E3E3E3;
+  background-color: #e3e3e3;
 `;
 
 const BoxCMainQRBox = styled.div`
@@ -288,7 +291,7 @@ const BoxCMainQRBox = styled.div`
   width: 100%;
   height: 85%;
   border-radius: 20px;
-`; 
+`;
 
 const BoxCMainQrBoxTag = styled.div`
   display: flex;
@@ -298,7 +301,7 @@ const BoxCMainQrBoxTag = styled.div`
   overflow: hidden;
   box-shadow: 0 2px 4px rgba(76, 76, 76, 0), 0 -2px 4px rgba(76, 76, 76, 0.1),
     2px 0 4px rgba(76, 76, 76, 0.1), -2px 0 4px rgba(76, 76, 76, 0.1);
-`
+`;
 const BoxCMainQr = styled.img`
   width: 100%;
   height: 100%;
@@ -324,7 +327,7 @@ const BoxDMain = styled.div`
   box-shadow: 0 2px 4px rgba(76, 76, 76, 0), 0 -2px 4px rgba(76, 76, 76, 0.1),
     2px 0 4px rgba(76, 76, 76, 0.1), -2px 0 4px rgba(76, 76, 76, 0.1);
 `;
-  
+
 const BoxDMainA = styled.div`
   display: flex;
   width: 100%;
@@ -362,4 +365,4 @@ const BoxDMainC = styled.div`
   height: 30px;
   margin-bottom: 5px;
   border-radius: 20px;
-`
+`;
