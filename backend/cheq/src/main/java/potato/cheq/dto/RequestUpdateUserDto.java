2c0d942609322ba67806fe67192831c828cbe7be
seonghun <sdzx0719@naver.com>
2024-05-31 12:40:08 +0900

    fix(service): device 추가 dto 오류 수정

--- backend/cheq/src/main/java/potato/cheq/dto/RequestUpdateUserDto.java
+++ backend/cheq/src/main/java/potato/cheq/dto/RequestUpdateUserDto.java
@@ -1,11 +1,15 @@
 package potato.cheq.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestUpdateUserDto {
     private String uuid;
 }
