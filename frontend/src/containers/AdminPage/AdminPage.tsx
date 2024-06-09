b604936b39a414be107c93a7f7e9efda9a0f895d
jiho0419 <127321491+jiho0419@users.noreply.github.com>
2024-06-09 22:20:58 +0900

    관리자페이지 등록,삭제, 로그아웃, 검색 기능 구현  #24

--- frontend/src/containers/AdminPage/AdminPage.tsx
+++ frontend/src/containers/AdminPage/AdminPage.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { UseFormReturn, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom"; // useNavigate 훅 임포트
 
 interface AdminData {
@@ -14,6 +14,11 @@ interface AdminData {
 
 const AdminPage = () => {
   const [adminData, setAdminData] = useState<AdminData[]>([]);
+  const [filteredData, setFilteredData] = useState<AdminData[]>([]);
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
+  const [showRegistrationModal, setShowRegistrationModal] = useState(false);
+  const navigate = useNavigate();
+
   const TestData: AdminData[] = [
     {
       checkbox: false,
@@ -48,18 +53,77 @@ const AdminPage = () => {
       chapelseat: "D34",
     },
     {
-      checkbox: true,
+      checkbox: false,
       name: "남궁지수",
       studentid: 202110651,
       studentclass: "미디어영상광고학",
       chapel: 7,
       chapelseat: "E56",
     },
+    {
+      checkbox: false,
+      name: "송태진",
+      studentid: 202110034,
+      studentclass: "컴퓨터공학",
+      chapel: 3,
+      chapelseat: "A01",
+    },
   ];
 
   useEffect(() => {
     setAdminData(TestData);
+    setFilteredData(TestData);
   }, []);
+
+  const handleLogout = () => {
+    setShowLogoutModal(true);
+  };
+
+  const handleConfirmLogout = () => {
+    navigate("/");
+  };
+
+  const handleCancelLogout = () => {
+    setShowLogoutModal(false);
+  };
+
+  const handleDelete = () => {
+    const newData = adminData.filter((item) => !item.checkbox);
+    setAdminData(newData);
+    setFilteredData(newData);
+  };
+
+  const { register, handleSubmit, reset } = useForm();
+  // const newStudent: AdminData = {
+  //   checkbox: false,
+  //   name: data.name,
+  //   studentid: parseInt(data.studentid),
+  //   studentclass: data.studentclass,
+  //   chapel: parseInt(data.chapel),
+  //   chapelseat: data.chapelseat,
+  // };
+  // const newData = [...adminData, newStudent];
+  // setAdminData(newData);
+  // setFilteredData(newData);
+  // reset(); // 입력 폼 초기화
+  // setShowRegistrationModal(false); // 등록 모달 닫기
+  // //
+
+  const onSubmit = (data: any) => {
+    const searchQuery = data.search.toLowerCase();
+    const filtered = adminData.filter((student) => {
+      const chapelWithSuffix = `${student.chapel}교시`;
+      return (
+        student.name.toLowerCase().includes(searchQuery) ||
+        student.studentid.toString().includes(searchQuery) ||
+        student.studentclass.toLowerCase().includes(searchQuery) ||
+        student.chapel.toString().includes(searchQuery) ||
+        chapelWithSuffix.includes(searchQuery) ||
+        student.chapelseat.toLowerCase().includes(searchQuery)
+      );
+    });
+    setFilteredData(filtered);
+  };
 
   return (
     <div>
@@ -69,26 +133,41 @@ const AdminPage = () => {
           <SubTitle>관리자 페이지</SubTitle>
         </AdminTitle>
         <AdminManagementBox>
-          <RegistrationBtn>등록</RegistrationBtn>
-          <CorrectionBtn>수정</CorrectionBtn>
-          <DeleteBtn>삭제</DeleteBtn>
-          <LogoutBtn>로그아웃</LogoutBtn>
-          <SearchBar>검색하기</SearchBar>
+          <AdminMangementBtnBox>
+            <RegistrationBtn onClick={() => setShowRegistrationModal(true)}>
+              등록
+            </RegistrationBtn>
+            <CorrectionBtn>수정</CorrectionBtn>
+            <DeleteBtn onClick={handleDelete}>삭제</DeleteBtn>
+            <LogoutBtn onClick={handleLogout}>로그아웃</LogoutBtn>
+          </AdminMangementBtnBox>
+          <AdminMangementSearchBox>
+            <SearchBar>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <SearchInput
+                  type="text"
+                  placeholder="원하는 정보를 입력하세요"
+                  {...register("search")}
+                />
+                <SearchButton type="submit">검색</SearchButton>
+              </form>
+            </SearchBar>
+          </AdminMangementSearchBox>
         </AdminManagementBox>
         <AdminInfo>
-          <SutudentTitleBox>
+          <StudentTitleBox>
             <StdTitleBox1>
-              <SutudentInfoTitle>이름</SutudentInfoTitle>
-              <SutudentInfoTitle>학번</SutudentInfoTitle>
+              <StudentInfoTitle>이름</StudentInfoTitle>
+              <StudentInfoTitle>학번</StudentInfoTitle>
             </StdTitleBox1>
-            <SutudentInfoTitle3>학과</SutudentInfoTitle3>
+            <StudentInfoTitle3>학과</StudentInfoTitle3>
             <StdTitleBox2>
-              <SutudentInfoTitle2>채플</SutudentInfoTitle2>
-              <SutudentInfoTitle2>좌석</SutudentInfoTitle2>
+              <StudentInfoTitle2>채플</StudentInfoTitle2>
+              <StudentInfoTitle2>좌석</StudentInfoTitle2>
             </StdTitleBox2>
-          </SutudentTitleBox>
+          </StudentTitleBox>
           <StudentInfo>
-            {adminData.map((data, index) => (
+            {filteredData.map((data, index) => (
               <StudentInfoRow key={index}>
                 <ClickBox
                   type="checkbox"
@@ -109,18 +188,214 @@ const AdminPage = () => {
           </StudentInfo>
         </AdminInfo>
       </StyleAdminPage>
+      {showLogoutModal && (
+        <LogoutModal>
+          <ModalContent>
+            <ModalMessage>로그아웃 하시겠습니까?</ModalMessage>
+            <ModalButtonBox>
+              <ModalButton onClick={handleConfirmLogout}>Yes</ModalButton>
+              <ModalButton onClick={handleCancelLogout}>No</ModalButton>
+            </ModalButtonBox>
+          </ModalContent>
+        </LogoutModal>
+      )}
+      {showRegistrationModal && (
+        <RegistrationModal>
+          <ModalContent>
+            <ModalTitle>학생 등록</ModalTitle>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <InputContainer>
+                <InputLabel>이름:</InputLabel>
+                <InputField {...register("name")} />
+              </InputContainer>
+              <InputContainer>
+                <InputLabel>학번:</InputLabel>
+                <InputField type="number" {...register("studentid")} />
+              </InputContainer>
+              <InputContainer>
+                <InputLabel>학과:</InputLabel>
+                <InputField {...register("studentclass")} />
+              </InputContainer>
+              <InputContainer>
+                <InputLabel>채플:</InputLabel>
+                <InputField type="number" {...register("chapel")} />
+              </InputContainer>
+              <InputContainer>
+                <InputLabel>좌석:</InputLabel>
+                <InputField {...register("chapelseat")} />
+              </InputContainer>
+              <ModulButtonBox>
+                <ModalButton type="submit">등록</ModalButton>
+                <ModalButton onClick={() => setShowRegistrationModal(false)}>
+                  취소
+                </ModalButton>
+              </ModulButtonBox>
+            </form>
+          </ModalContent>
+        </RegistrationModal>
+      )}
     </div>
   );
 };
 
 export default AdminPage;
+const RegistrationModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 30%;
+  padding: 20px;
+  background-color: white;
+  border: 1px solid #375cde;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-const AdminManagementBox = styled.div``;
-const RegistrationBtn = styled.button``;
-const CorrectionBtn = styled.button``;
-const DeleteBtn = styled.button``;
-const LogoutBtn = styled.button``;
-const SearchBar = styled.div``;
+const ModalTitle = styled.h2`
+  font-size: 18px;
+  margin-bottom: 20px;
+`;
+
+const InputContainer = styled.div`
+  margin-bottom: 15px;
+`;
+
+const InputLabel = styled.label`
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+const InputField = styled.input`
+  padding: 5px;
+  font-size: 14px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  width: 100%;
+`;
+const ModulButtonBox = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+//
+const ModalContent = styled.div`
+  display: flex;
+  padding: 20px;
+  border-radius: 10px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LogoutModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -100%);
+  width: 20%;
+  height: 20%;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 20px;
+  background-color: white;
+  border: 1px solid #375cde;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ModalMessage = styled.p`
+  font-size: 15px;
+  margin-bottom: 20px;
+`;
+
+const ModalButtonBox = styled.div`
+  display: flex;
+  font-size: 13px;
+`;
+
+const ModalButton = styled.button`
+  padding: 10px 20px;
+  margin: 0 10px;
+  border-radius: 5px;
+  border: none;
+  background-color: #375cde;
+  color: white;
+  cursor: pointer;
+`;
+
+const AdminManagementBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const AdminMangementBtnBox = styled.div``;
+const AdminMangementSearchBox = styled.div``;
+const RegistrationBtn = styled.button`
+  padding: 5px 13px;
+  border-radius: 10px;
+  font-size: 12px;
+  border: none;
+  color: white;
+  background-color: #375cde;
+  margin-right: 10px;
+  cursor: pointer;
+`;
+const CorrectionBtn = styled.button`
+  padding: 5px 13px;
+  border-radius: 10px;
+  font-size: 12px;
+  border: none;
+  color: white;
+  background-color: #375cde;
+  margin-right: 10px;
+  cursor: pointer;
+`;
+const DeleteBtn = styled.button`
+  padding: 5px 13px;
+  border-radius: 10px;
+  font-size: 12px;
+  border: none;
+  color: white;
+  background-color: #375cde;
+  margin-right: 10px;
+  cursor: pointer;
+`;
+const LogoutBtn = styled.button`
+  padding: 5px 13px;
+  border-radius: 10px;
+  font-size: 12px;
+  border: none;
+  color: white;
+  background-color: #375cde;
+  cursor: pointer;
+`;
+const SearchBar = styled.div`
+  form {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const SearchInput = styled.input`
+  padding: 5px;
+  font-size: 14px;
+  border: none;
+  border-bottom: 2px solid #375cde;
+  margin-right: 5px;
+  outline: none;
+`;
+
+const SearchButton = styled.button`
+  padding: 5px 10px;
+  font-size: 14px;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  background-color: #375cde;
+  cursor: pointer;
+`;
 
 const StyleAdminPage = styled.div`
   display: flex;
@@ -147,7 +422,6 @@ const SubTitle = styled.h3`
 const AdminInfo = styled.div`
   display: flex;
   flex-direction: column;
-
   margin-top: 20px;
   box-shadow: 0 2px 4px rgba(76, 76, 76, 0), 0 -2px 4px rgba(76, 76, 76, 0.1),
     2px 0 4px rgba(76, 76, 76, 0.1), -2px 0 4px rgba(76, 76, 76, 0.1);
@@ -155,7 +429,7 @@ const AdminInfo = styled.div`
   border-radius: 30px;
 `;
 
-const SutudentTitleBox = styled.div`
+const StudentTitleBox = styled.div`
   display: flex;
   flex-direction: row;
   padding: 16px 0px;
@@ -169,21 +443,21 @@ const StdTitleBox1 = styled.div`
   display: flex;
   font-size: 16px;
   font-weight: bold;
-  margin-left: 46px;
+  margin-left: 48px;
 `;
 const StdTitleBox2 = styled.div`
   display: flex;
   font-size: 16px;
   font-weight: bold;
-  margin-left: 95px;
+  margin-left: 100px;
 `;
-const SutudentInfoTitle = styled.div`
+const StudentInfoTitle = styled.div`
   margin-right: 50px;
 `;
-const SutudentInfoTitle2 = styled.div`
+const StudentInfoTitle2 = styled.div`
   margin-left: 30px;
 `;
-const SutudentInfoTitle3 = styled.div`
+const StudentInfoTitle3 = styled.div`
   margin-left: 28px;
   font-size: 16px;
   font-weight: bold;
