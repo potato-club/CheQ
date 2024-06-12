7d49c0d3a2d82c521891741fb0e7c5da956a58dd
seonghun <sdzx0719@naver.com>
2024-06-12 17:49:16 +0900

    add(error): nfc, beacon 출결시 기기정보 일치하지 않으면 오류출력

--- backend/cheq/src/main/java/potato/cheq/controller/AttendanceController.java
+++ backend/cheq/src/main/java/potato/cheq/controller/AttendanceController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import potato.cheq.dto.request.BeaconRequestDto;
 import potato.cheq.dto.request.NFCRequestDto;
+import potato.cheq.error.security.ErrorCode;
+import potato.cheq.error.security.requestError.UnAuthorizedException;
 import potato.cheq.service.AttendanceService;
 import potato.cheq.service.jwt.JwtTokenProvider;
 
@@ -25,6 +27,9 @@ public class AttendanceController {
     @Operation(summary = "nfc 출결 API")
     public ResponseEntity<String> checkAttendanceByNFC(HttpServletRequest request, @RequestBody NFCRequestDto nfcRequestDto) throws Exception {
         String uuid = attendanceService.checkAttendanceByNfcService(request, nfcRequestDto);
+        if (uuid == null) {
+            throw new UnAuthorizedException("기기 정보가 일치하지 않습니다.", ErrorCode.INVALID_TOKEN_EXCEPTION);
+        }
         return ResponseEntity.ok().body(uuid + " 기기 NFC 태그");
     }
 
@@ -32,6 +37,9 @@ public class AttendanceController {
     @Operation(summary = "beacon 출결 API")
     public ResponseEntity<String> checkAttendanceByBeacon(HttpServletRequest request, @RequestBody BeaconRequestDto beaconRequestDto) throws Exception {
         String uuid = attendanceService.checkAttendanceByBeaconService(request, beaconRequestDto);
+        if (uuid == null) {
+            throw new UnAuthorizedException("기기 정보가 일치하지 않습니다.", ErrorCode.INVALID_TOKEN_EXCEPTION);
+        }
         return ResponseEntity.ok().body(uuid + " 기기 Beacon 출결");
     }
 
