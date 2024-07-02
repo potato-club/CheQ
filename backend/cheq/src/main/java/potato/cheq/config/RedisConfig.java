af351982391d5096a72ce7587291534b09e0d81f
Teddysir <sdzx0719@naver.com>
2024-07-02 19:30:46 +0900

    fix(yml): redis port, host 수정

--- backend/cheq/src/main/java/potato/cheq/config/RedisConfig.java
+++ backend/cheq/src/main/java/potato/cheq/config/RedisConfig.java
@@ -6,17 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Bean
